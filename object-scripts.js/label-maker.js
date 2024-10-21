9ce8afbe0b9b156ce4d3e278f0f45b18eec5083d
Nicholas Lynch <nicklynch820@gmail.com>
2024-10-21 11:49:28 -0400

    mail label made

---
+++ object-scripts.js/label-maker.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let myInfo = {
+  name: "Nick Lynch",
+  address: "123 paint street",
+  city: "Bronx",
+  state: "New York",
+  zip: "10003",
+};
+
+function printContact(myInfo) {
+  console.log(myInfo.name);
+  console.log(myInfo.address);
+  console.log(myInfo.city + "," + " " + myInfo.state + " " + myInfo.zip);
+}
+printContact(myInfo);
