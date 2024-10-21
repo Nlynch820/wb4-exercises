3647a9ca57f97427deabaac9a7ec27e39330abdb
Nicholas Lynch <nicklynch820@gmail.com>
2024-10-21 15:09:15 -0400

    wb4 pg2-9 exercises dones

---
+++ object-scripts.js/loop-exercises.js
@@ -0,0 +1,14 @@
+"use strict"
+// let message = 0;
+
+// while (message <= 6) {
+//     console.log("I Love Loops");
+//     message = message +1;
+// }
+
+let message = "I Love Loops";
+for(let loop = 0; loop <= 6; loop = loop + 1){
+message = message;
+console.log(message);
+
+}
