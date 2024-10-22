0a0804105f6413c6de2918e9103d57fda320ab7c
Nicholas Lynch <nicklynch820@gmail.com>
2024-10-22 09:38:58 -0400

    my family loop

---
+++ exercises.js/my-family.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let brothers = [
+    { name: "Nick" }, 
+    { name: "Joseph" }, 
+    { name: "Ryan" }, 
+    { name: "Michael"}
+];
+
+for(let i = 0; i < brothers.length; i++) {
+    let family = brothers[i];
+    console.log(family.name);
+}
+
