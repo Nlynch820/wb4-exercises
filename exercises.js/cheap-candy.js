b5fb4449c150d5e4a7b32e26f0ecd105105e4896
Nicholas Lynch <nicklynch820@gmail.com>
2024-10-23 13:46:09 -0400

    cheap candies exercises works

--- exercises.js/cheap-candy.js
+++ exercises.js/cheap-candy.js
@@ -1,26 +1,25 @@
-"use strict"
+"use strict";
 
 let products = [
-    {product: "Gummy Worms", price: 5.79},
-    {product: "Plain M&Ms", price: 2.89},
-    {product: "Peanut M&Ms", price: 2.89},
-    {product: "Swedish Fish", price: 3.79},
-    // TODO: fill the array with 10 candies of various
-    // price ranges
-   ];
+  { product: "Gummy Worms", price: 5.79 },
+  { product: "Plain M&Ms", price: 2.89 },
+  { product: "Peanut M&Ms", price: 2.89 },
+  { product: "Swedish Fish", price: 3.79 },
+  { product: "Sour Patch Kids", price: 6.82 },
+  { product: "Skittles", price: 4.79 },
+];
 
-   for(let product of products){
-    if (product.price < 4)
-        console.log(product.product);
-   }
+for (let product of products) {
+  if (product.price < 4);
+  console.log(product.product);
+}
 
-   for (let product of products){
-    if (product.product == "M&Ms")
-        console.log(product.product);
-        
-   }
+for (let product of products) {
+  if (product.product.includes("M&Ms"));
+  console.log(product.product);
+}
 
-   for (let product of products){
-    if(product.product == "Swedish Fish")
-        console.log("Yes we carry Swedish Fish");   
-   }
+for (let product of products) {
+  if (product.product == "Swedish Fish");
+  console.log("Yes we carry Swedish Fish");
+}
